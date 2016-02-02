c6fa753271964fab3d6fbec566adab536b7cc5b1
Jearvon Dharrie <j.dharrie@gmail.com>
2016-02-02 02:21:24 -0500

    wip

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.julienxx/clj-slack "0.4.2"]
+                 [org.julienxx/clj-slack "0.5.0"]
                  [org.clojure/data.json "0.2.5"]
                  [stylefruits/gniazdo "0.3.1"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
